a280b9a9b829115e8a0252db42f09a1990f97f5a
iftikhrlutfur <iftikherlutfur@gmail.com>
2024-08-17 15:27:18 +0600

    initial setup of firebase

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,12 +3,18 @@ import {
   } from "react-router-dom";
 import Root from "./Root";
 import Home from "./Comonents/Home";
+import Register from "./Comonents/Authentication/Register";
 
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Root/>,
       children:[
+        {
+          path:'/',
+          element:<Register/>
+
+        },
         {
             path:'/categories',
             element:<Home/>
